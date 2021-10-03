0e10fd52fac4601c562ffd22e30668444ef95b20
vincentzyc <vincentzyc@qq.com>
2021-10-03 12:06:35 +0800

    init: init

---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+Page({
+    data: {
+        url: 'https://h5.liulianglf.cn/h5/index.html?id=2021081816082400197&pid=16800',
+    },
+    onLoad: function () {
+        const options = ks.getLaunchOptionsSync();
+        if (options.query) {
+            const h5page = options.query.h5page || ''
+            const callback = options.query.callback || ''
+            let nurl = this.data.url
+            if (h5page) nurl = h5page
+            if (callback) nurl = nurl.includes('?') ? nurl + '&callback=' + callback : nurl + '?callback=' + callback
+            this.setData({ url: nurl });
+        }
+    }
+})
