b1bb179951108bbcfae0491388f2657f63533e59
vincentzyc <vincentzyc@qq.com>
2021-10-05 19:28:18 +0800

    🎉 init: init

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,21 +1,19 @@
-//index.js
 Page({
     data: {
         hadInit: true,
-        url: 'https://h5.liulianglf.cn/h5/index.html?id=2021081816082400197&pid=16800',
+        // url: 'https://h5.liulianglf.cn/h5/index.html?id=2021081816082400197&pid=16800',
     },
     onLoad: function () {
-        const options = ks.getLaunchOptionsSync();
-        if (options.query) {
-            const h5page = options.query.h5page || ''
-            const callback = options.query.callback || ''
-            let nurl = this.data.url
-            if (h5page) nurl = h5page
-            if (callback) nurl = nurl.includes('?') ? nurl + '&callback=' + callback : nurl + '?callback=' + callback
-            this.setData({ url: nurl });
-        }
+        // const options = ks.getLaunchOptionsSync();
+        // if (options.query) {
+        //     const h5page = options.query.h5page || ''
+        //     const callback = options.query.callback || ''
+        //     let nurl = this.data.url
+        //     if (h5page) nurl = h5page
+        //     if (callback) nurl = nurl.includes('?') ? nurl + '&callback=' + callback : nurl + '?callback=' + callback
+        //     this.setData({ url: nurl });
+        // }
         setTimeout(() => {
-            console.log(111)
             this.setData({ hadInit: false });
         }, 1000)
     }
