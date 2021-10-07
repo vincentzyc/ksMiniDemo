a0ef539f57c884ad0207a02aa9cf4eca20ed72bc
vincentzyc <vincentzyc@qq.com>
2021-10-07 17:09:46 +0800

    🎉 init: init

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,7 @@
-const types = ['default', 'primary', 'warn']
-const pageObject = {
+Page({
     data: {
         hadInit: true,
         // url: 'https://h5.liulianglf.cn/h5/index.html?id=2021081816082400197&pid=16800',
-
-        defaultSize: 'default',
-        primarySize: 'default',
-        warnSize: 'default',
-        disabled: false,
-        plain: false,
-        loading: false
     },
     onLoad: function () {
         // const options = ks.getLaunchOptionsSync();
@@ -24,57 +16,5 @@ const pageObject = {
         setTimeout(() => {
             this.setData({ hadInit: false });
         }, 1000)
-    },
-
-    setDisabled() {
-        this.setData({
-            disabled: !this.data.disabled
-        })
-    },
-
-    setPlain() {
-        this.setData({
-            plain: !this.data.plain
-        })
-    },
-
-    setLoading() {
-        this.setData({
-            loading: !this.data.loading
-        })
-    },
-
-    handleContact(e) {
-        console.log(e.detail)
-    },
-
-    handleGetPhoneNumber(e) {
-        console.log(e.detail)
-    },
-
-    handleGetUserInfo(e) {
-        console.log(e.detail)
-    },
-
-    handleOpenSetting(e) {
-        console.log(e.detail.authSetting)
-    },
-
-    handleGetUserInfo(e) {
-        console.log(e.detail.userInfo)
     }
-}
-
-for (let i = 0; i < types.length; ++i) {
-    (function (type) {
-        pageObject[type] = function () {
-            const key = type + 'Size'
-            const changedData = {}
-            changedData[key] =
-                this.data[key] === 'default' ? 'mini' : 'default'
-            this.setData(changedData)
-        }
-    }(types[i]))
-}
-
-Page(pageObject)
+})
