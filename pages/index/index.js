4252536f620ef6f0700e47d50071224e478b45f0
vincentzyc <vincentzyc@qq.com>
2021-10-09 09:13:53 +0800

    🎉 init: init

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,20 +1,29 @@
 Page({
     data: {
-        hadInit: true,
+        // hadInit: true,
         // url: 'https://h5.liulianglf.cn/h5/index.html?id=2021081816082400197&pid=16800',
+    },
+    goOrder() {
+        ks.navigateTo({
+            url: '/pages/iframe/iframe?url=' + encodeURIComponent('https://card.liulianglf.cn/sim/index.html#/login'),
+        })
+    },
+    openService() {
+        const elYunPopup = this.selectComponent('#yun-popup')
+        elYunPopup.data.show ? this.selectComponent('#yun-popup').closePopup() : this.selectComponent('#yun-popup').openPopup()
     },
     onLoad: function () {
         // const options = ks.getLaunchOptionsSync();
         // if (options.query) {
         //     const h5page = options.query.h5page || ''
         //     const callback = options.query.callback || ''
-        //     let nurl = this.data.url
+        //     let nurl = this.data.url`
         //     if (h5page) nurl = h5page
         //     if (callback) nurl = nurl.includes('?') ? nurl + '&callback=' + callback : nurl + '?callback=' + callback
         //     this.setData({ url: nurl });
         // }
-        setTimeout(() => {
-            this.setData({ hadInit: false });
-        }, 1000)
+        // setTimeout(() => {
+        //     this.setData({ hadInit: false });
+        // }, 1000)
     }
 })
